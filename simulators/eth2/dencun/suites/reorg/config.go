be028cc71c2d02ad34c60a93ea11c77b32907707
Mario Vega <marioevz@gmail.com>
2023-09-28 22:49:11 +0000

    simulators/eth2/dencun: Refactor test waits

--- simulators/eth2/dencun/suites/reorg/config.go
+++ simulators/eth2/dencun/suites/reorg/config.go
@@ -6,15 +6,10 @@ import (
 	"github.com/ethereum/hive/simulators/eth2/common/clients"
 	"github.com/ethereum/hive/simulators/eth2/common/testnet"
 	suite_base "github.com/ethereum/hive/simulators/eth2/dencun/suites/base"
-	beacon "github.com/protolambda/zrnt/eth2/beacon/common"
 )
 
 type ReorgTestSpec struct {
 	suite_base.BaseTestSpec
-
-	// Number of epochs to wait for clients to build the separate chains before starting the last
-	// client that re-orgs all clients to a canonical chain
-	EpochsToSync beacon.Epoch
 
 	// Array of weights for each chain, the last client re-orgs to the chain with the highest weight.
 	// The length of the array also determines the number of chains to generate.
